25db292497bd3c4c6215710133ce7bde3f3c4271
kushanxp <win32.sysP@88w0rd>
2024-01-30 17:33:10 -0500

    initial commit

--- accounts/src/main/java/com/eazybytes/accounts/config/ConfigProperties.java
+++ accounts/src/main/java/com/eazybytes/accounts/config/ConfigProperties.java
@@ -1,10 +1,17 @@
 package com.eazybytes.accounts.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record ConfigProperties(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Data
+public class ConfigProperties {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
--- loans/src/main/java/com/eazybytes/loans/config/ConfigProperties.java
+++ loans/src/main/java/com/eazybytes/loans/config/ConfigProperties.java
@@ -1,10 +1,16 @@
 package com.eazybytes.loans.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record ConfigProperties(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Data
+public class ConfigProperties {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
