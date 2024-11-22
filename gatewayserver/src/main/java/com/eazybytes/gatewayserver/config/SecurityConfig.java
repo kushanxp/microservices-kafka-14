62e881654c498ef09a5d3eeb3808cb2c0fb7f761
kushanxp <win32.sysP@88w0rd>
2024-11-22 14:26:22 -0500

    OAuth2 config added.

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.eazybytes.gatewayserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("/eazybank/accounts/**").authenticated()
+                .pathMatchers("/eazybank/loans/**").authenticated()
+                .pathMatchers("/eazybank/cards/**").authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return serverHttpSecurity.build();
+
+    }
+}
