a5daaf75ea95b9ad463944ddd5fedd3af3f2754f
kushanxp <win32.sysP@88w0rd>
2024-11-22 18:33:51 -0500

    KeyCloak configured as the AuthServer .

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/WebFluxSecurityConfig.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/WebFluxSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.gatewayserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -7,10 +8,12 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
 
+@Configuration
+@EnableWebFluxSecurity()
+public class WebFluxSecurityConfig {
+
+    @Bean(name = "webFluxSecurityConfigFilterChain")
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
                 .pathMatchers("/eazybank/accounts/**").authenticated()
